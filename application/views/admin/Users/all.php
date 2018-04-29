8e2ac2e195a2e2651a073c893be789260d9d1d5a
TheeLiFang <theelifang@gmail.com>
2018-04-29 20:11:30 +0800

    Update user part

--- application/views/admin/Users/all.php
+++ application/views/admin/Users/all.php
@@ -1,7 +1,7 @@
 <section class="content-header padding-15">
-    <h1>Users</h1>
+    <h1>User</h1>
     <ol class="breadcrumb">
-        <li><a href="<?= base_url() ?>userList"><i class="fa fa-user"></i>Users</a></li>
+        <li><a href="<?= base_url() ?>userList"><i class="fa fa-user"></i>User</a></li>
     </ol>
 </section>
 <br>
@@ -9,7 +9,9 @@
     <div class="mediumBox">
         <div class='panel panel-default'>
             <div class='panel-heading'>
-                <h4 class="whiteTitle" style='display: inline-block;'>Users</h4>
+                <h4 class="whiteTitle" style='display: inline-block;'>User</h4>
+                <a href='<?php echo site_url('userList/add'); ?>' class='btn btn-info pull-right'>
+                    <i class='glyphicon glyphicon-plus' ></i>Add</a>
             </div>
             <div class='panel-body'>
                 <div id="refreshBox">
@@ -19,6 +21,9 @@
                                 <th>No.</th>
                                 <th>Name</th>
                                 <th>Email</th>
+                                <th>Country</th>
+                                <th>Bank Name</th>
+                                <th>Bank Acc. No.</th>
                                 <th></th>
                             </tr>
                         </thead>
@@ -28,11 +33,27 @@
                             foreach ($users as $row) {
                                 ?>
                                 <tr>
-                                    <td><a href="<?= base_url() ?>users/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $i; ?></a></td>
-                                    <td><a href="<?= base_url() ?>users/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['username']; ?></a></td>
-                                    <td><a href="<?= base_url() ?>users/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['email']; ?></a></td>
-                                    <td><button class="btn btn-danger delete-button" data-id="<?= $row['user_id'] ?>"
-                                            <i class="fa fa-trash"></i>Delete</button></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $i; ?></a></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['username']; ?></a></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['email']; ?></a></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['country']; ?></a></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['bank_name']; ?></a></td>
+                                    <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend"><?php echo $row['bank_account_number']; ?></a></td>
+                                    <?php
+                                        if ($row['deleted'] == "0"){
+                                            ?>
+                                            <td><button class="btn btn-danger delete-button" data-id="<?= $row['user_id'] ?>"
+                                                <i class="fa fa-trash"></i>Block</button></td>
+                                    <?php
+                                        }
+                                        ?>
+                                    <?php
+                                        if ($row['deleted'] == "1") {
+                                            ?>
+                                                <td><a href="<?= base_url() ?>userList/details/<?= $row['user_id'] ?>" class="a-color-backend">Blocked</a></td>
+                                    <?php
+                                        }
+                                        ?>
                                 </tr>
                                 <?php
                                 $i++;
@@ -44,6 +65,9 @@
                                 <th>No.</th>
                                 <th>Name</th>
                                 <th>Email</th>
+                                <th>Country</th>
+                                <th>Bank Name</th>
+                                <th>Bank Acc. No.</th>
                                 <th></th>
                             </tr>
                         </tfoot>
@@ -53,3 +77,11 @@
         </div>
     </div>
 </section>
+<script>
+    $(document).on('click', '.delete-button', function (e) {
+        if (confirm("Are you sure you want to delete this user?")) {
+            id = $(this).attr('data-id');
+            window.location.replace("<?= base_url() ?>userList/delete/" + id);
+        }
+    });
+</script>
--- application/views/admin/Users/details.php
+++ application/views/admin/Users/details.php
@@ -3,8 +3,13 @@
     <div class="row">
         <div class="col-lg-6 col-md-6 col-xs-12 col-sm-12">
             <div class="box box-info">
+                <div class="box-header admin-panel">
+                    <a href="<?= site_url("userList/edit/" . $user['user_id']); ?>" class="pull-right a-color-backend">
+                        Edit
+                    </a>
+                    <h4 style="margin-left:20px;" class="pull-left"><?= $user['username']?>'s Info</h4>
+                </div>
                 <div class="box-body">
-
                     <div class="nav-tabs-custom">
                         <ul class="nav nav-tabs" id="tabs">
                             <li class="active">
@@ -18,16 +23,24 @@
                         <div class="tab-pane active backgroundcolor-white" id="login">
                             <table class='formTable'>
                                 <tr>
-                                    <th>Amount</th>
-                                    <td>: <?= $transaction['amount']; ?></td>
+                                    <th>Username</th>
+                                    <td>: <?= $user['username']; ?></td>
                                 </tr>
                                 <tr>
-                                    <th>Amount</th>
-                                    <td>: <?= $transaction['description']; ?></td>
+                                    <th>Email</th>
+                                    <td>: <?= $user['email']; ?></td>
                                 </tr>
                                 <tr>
-                                    <th>Amount</th>
-                                    <td>: <?= $transaction['remark']; ?></td>
+                                    <th>Country</th>
+                                    <td>: <?= $user['country']; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>Bank Name</th>
+                                    <td>: <?= $user['bank_name']; ?></td>
+                                </tr>
+                                <tr>
+                                    <th>Bank Acc. No.</th>
+                                    <td>: <?= $user['bank_account_number']; ?></td>
                                 </tr>
                             </table>
                         </div>
