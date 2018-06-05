50eede15b150d47a62e70341b3f3d0cf7451af26
emmwee <emmwee96@gmail.com>
2018-06-05 13:49:50 +0800

    user listing details

--- application/views/main/details.php
+++ application/views/main/details.php
@@ -1,67 +1,70 @@
 <!-- Main content -->
-<section class="content col-lg-12 col-md-12 col-xs-12 col-sm-12">
-	<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding border-bottom margin-bottom-10">
-		<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3 sub-padding">
-				<span class="font-weight-bold">User:</span>
-				<?= $user_listing["username"] ?>
-			</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 sub-padding font-color-user">
-				Average Release Time:
-				<?= $user_listing["average_time"] ?> mins
+<section class="content col-xs-12">
+	<div class="col-xs-12 sub-padding border-bottom margin-bottom-10">
+		<div class="col-sm-6">
+			<div class="row">
+				<div class="col-md-6 col-xs-12 sub-padding">
+					<span class="font-weight-bold">User:</span>
+					<?= $user_listing["username"] ?>
+				</div>
+				<div class="col-xs-12 sub-padding font-color-user">
+					<span class="font-weight-bold">Average Release Time:</span>
+					<?= $user_listing["average_time"] ?> mins
+				</div>
 			</div>
 		</div>
-		<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3 sub-padding">
+		<div class="col-md-6 col-xs-12">
+			<div class="col-xs-3 sub-padding">
 				<?= $user_listing["trades"] ?> Trades
 			</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3 sub-padding">
+			<div class="col-xs-3 sub-padding">
 				<?= $user_listing["trusted"] ?> Trusted
 			</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3 sub-padding">
+			<div class="col-xs-3 sub-padding">
 				<?= $user_listing["rating"] ?>% Rating
 			</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3 sub-padding">
-				<?= $user_listing["amount"] ?> BTC Volume
+			<div class="col-xs-3 sub-padding">
+				<?= $user_listing["amount"] ?>
+					<?= $user_listing["crypto"] ?> Volume
 			</div>
 		</div>
 	</div>
 
-	<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
-		<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding margin-bottom-10 font-weight-bold">
+	<div class="col-md-6 col-xs-12 no-padding no-margin">
+		<div class="col-xs-12 sub-padding margin-bottom-10 font-weight-bold">
 			Purchase Details
 		</div>
-		<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">MYR Amount:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+		<div class="col-xs-12 sub-padding">
+			<div class="col-xs-6 label-height col-padding-10">MYR Amount:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_trade["myr_amount"] ?> MYR</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">BTC Amount:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">BTC Amount:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_trade["btc_amount"] ?> BTC</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">Price:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">Price:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_listing["price_after"] ?> MYR/BTC</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">Limits:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">Limits:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_listing["limit_from"] ?>-
 					<?= $user_listing["limit_to"] ?> MYR</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">Payment Method:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">Payment Method:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_listing["payment_method"] ?>
 			</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">Time of Payment:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">Time of Payment:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_listing["time_of_payment"] ?> Minutes
 			</div>
-			<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">Status:</div>
-			<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+			<div class="col-xs-6 label-height col-padding-10">Status:</div>
+			<div class="col-xs-6 label-height col-padding-5">
 				<?= $user_trade["user_trade_status"] ?>
 			</div>
 			<?php
 		if ($user_trade['user_trade_status_id'] == 4 and empty($user_rating)) {
 			?>
-				<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">How would you rate this trade?</div>
-				<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+				<div class="col-xs-6 label-height col-padding-10">How would you rate this trade?</div>
+				<div class="col-xs-6 label-height col-padding-5">
 					<a href="<?= base_url() ?>user_listing/rate_trade/<?= $user_trade['user_listing_id'] ?>/<?= $user_trade['user_trade_id'] ?>/good"
 					class="btn btn-success">
 						<i class="fa fa-thumbs-up"></i>
@@ -75,34 +78,34 @@
 
 		} else if ($user_trade['user_trade_status_id'] == 4 and !empty($user_rating)) {
 			?>
-					<div class="col-lg-3 col-md-3 col-xs-3 col-sm-3label-height col-padding-10">You rate this trade:</div>
-					<div class="col-lg-9 col-md-9 col-xs-9 col-sm-9 label-height col-padding-5">
+					<div class="col-xs-6 label-height col-padding-10">You rate this trade:</div>
+					<div class="col-xs-6 label-height col-padding-5">
 						<?= strtoupper($user_rating['rating']) ?>
 					</div>
 					<?php
 
 			}
 			?>
-					<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding font-weight-bold">
+					<div class="col-xs-12 sub-padding font-weight-bold">
 						Messages
 					</div>
-					<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+					<div class="col-xs-12 sub-padding">
 						<?= (!empty($user_listing["message"])) ? $user_listing["message"] : "No Messages" ?>
 					</div>
-					<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding padding-top-50 font-weight-bold">
+					<div class="col-xs-12 sub-padding padding-top-50 font-weight-bold">
 						Notice of Transaction
 					</div>
-					<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+					<div class="col-xs-12 sub-padding">
 						Trading Currencies and other financial instruments carries risk due to the potential of increased volatility and speculation.
 					</div>
 		</div>
 	</div>
-	<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
+	<div class="col-md-6 col-xs-12 no-padding no-margin">
 		<?php
 	if ($user_trade["user_trade_status_id"] < 3) {
 		if (!empty($user_trade["receipt"])) {
 			?>
-			<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 font-weight-bold align-center">
+			<div class="col-xs-12 font-weight-bold align-center">
 				<a href="#img">
 					<img class="receipt" src="<?= base_url() . $user_trade['receipt'] ?>">
 				</a>
@@ -111,11 +114,11 @@
 				?>
 					<form method="POST" action="<?= base_url() ?>user_listing/details/<?= $user_listing['user_listing_id'] ?>/<?= $user_trade['user_trade_id'] ?>"
 					enctype="multipart/form-data">
-						<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+						<div class="col-xs-12 sub-padding">
 							<input type="file" name="receipt">
 							<input type="hidden" name="upload" value="receipt">
 						</div>
-						<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+						<div class="col-xs-12 sub-padding">
 							<input type="submit" class="btn btn-info pull-right" value="Reupload Receipt">
 						</div>
 					</form>
@@ -130,17 +133,17 @@
 		?>
 				<form method="POST" action="<?= base_url() ?>user_listing/details/<?= $user_listing['user_listing_id'] ?>/<?= $user_trade['user_trade_id'] ?>"
 				enctype="multipart/form-data">
-					<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 font-weight-bold">
+					<div class="col-xs-12 font-weight-bold">
 						<h3>Receipt Required</h3>
 					</div>
 					<?php
 				if ($this->session->userdata("user")["user_id"] == $user_trade["buyer_id"]) {
 					?>
-						<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+						<div class="col-xs-12 sub-padding">
 							<input type="file" name="receipt">
 							<input type="hidden" name="upload" value="receipt">
 						</div>
-						<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12 sub-padding">
+						<div class="col-xs-12 sub-padding">
 							<input type="submit" class="btn btn-info pull-right" value="Upload Receipt">
 						</div>
 						<?php
@@ -150,13 +153,11 @@
 				</form>
 				<hr>
 				<?php
-			if ($user_trade['user_trade_status'] == 1) {
+			if ($user_trade['user_trade_status'] == 1 and $this->session->userdata("user")["user_id"] == $user_trade["buyer_id"]) {
 				?>
-					<?php
-
-			}
-			?>
-					<h3>Pay with PayPal</h3>
+				<div class="col-xs-12 font-weight-bold">
+					<h3>Receipt Required</h3>
+				</div>
 					<br>
 					<br>
 
@@ -165,7 +166,8 @@
 
 			}
 		}
-		?>
+	}
+	?>
 	</div>
 </section>
 <?php
