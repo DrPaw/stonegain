a35f8a72e4532cdf66e9717eb7083f5ce42febc3
emmwee <emmwee96@gmail.com>
2018-06-06 13:34:13 +0800

    external transfer save to db

--- application/controllers/Crypto.php
+++ application/controllers/Crypto.php
@@ -1,35 +1,47 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Crypto extends BaseController {
+class Crypto extends BaseController
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         $this->load->model("Crypto_model");
         $this->load->model("GDAX");
-        
+
         $this->page_data = array();
     }
 
-    function pollAddress(){
+    function pollAddress()
+    {
         // this is main wallets resources
         $resources = $this->db->get('account_resource')->result_array();
         $wallets = array();
-        foreach ($resources as $row){
+        foreach ($resources as $row) {
             $wallets[$row['currency']] = $row;
         }
 
-        $address = $this->db->get_where("user_deposit_address",array(
+        $address = $this->db->get_where("user_deposit_address", array(
             "active" => 1
         ))->result_array();
 
-        foreach($address as $row){
-            $transactions = $this->GDAX->getAddressTransactions($wallets[$row['currency']]['value'],$row['resource_id']);
+        foreach ($address as $row) {
+            $transactions = $this->GDAX->getAddressTransactions($wallets[$row['currency']]['value'], $row['resource_id']);
+
+            $successful_transfer = json_decode($transactions["data"], true);
+            $successful_transfer = $successful_transfer["data"];
+
+            if (!empty($successful_transfer)) {
+                //die(var_dump($successful_transfer[0]));
+                $this->GDAX->external_transaction_process($successful_transfer[0], $wallets[$row['currency']]['value'], $row['resource_id']);
+            }
 
             var_dump($transactions);
-            echo "<hr>";
+            echo "<hr/>";
+
         }
     }
 }
--- application/models/GDAX.php
+++ application/models/GDAX.php
@@ -1,8 +1,10 @@
 <?php
 
-class GDAX extends CI_Model{
+class GDAX extends CI_Model
+{
 
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
 
         $this->apiKey = "dOFb9XvIM0BRa4tA";
@@ -19,9 +21,10 @@ class GDAX extends CI_Model{
         - user sends request
         - server creates a 1 time use address
         - server will save the transaction value
-    */
+     */
 
-    function getAccounts(){
+    function getAccounts()
+    {
         $result = $this->get('accounts');
 
         $result = json_decode($result);
@@ -31,10 +34,11 @@ class GDAX extends CI_Model{
             "status" => "SUCCESS",
             "data" => $result->data
         );
-        
+
     }
 
-    function user(){
+    function user()
+    {
         $result = $this->get("user");
 
         $result = json_decode($result);
@@ -44,81 +48,86 @@ class GDAX extends CI_Model{
         );
     }
 
-    function listAddress($currency){
-        $accountResource = $this->db->get_where("account_resource",array(
+    function listAddress($currency)
+    {
+        $accountResource = $this->db->get_where("account_resource", array(
             "currency" => $currency
         ))->result_array();
 
-        if (!count($accountResource)){
+        if (!count($accountResource)) {
             return array(
                 "status" => "ERROR",
                 "message" => "Currency not found"
             );
         }
 
-        $path = "accounts/". $accountResource[0]['value']."/addresses";
+        $path = "accounts/" . $accountResource[0]['value'] . "/addresses";
         $result = $this->get($path);
 
         $result = json_decode($result);
         $addresses = $result->data;
-        
+
 
         return array('status' => 'SUCCESS', 'data' => $addresses);
     }
 
-    function createAddress($currency,$addressName){
-        $accountResource = $this->db->get_where("account_resource",array(
+    function createAddress($currency, $addressName)
+    {
+        $accountResource = $this->db->get_where("account_resource", array(
             "currency" => $currency
         ))->result_array();
 
-        if (!count($accountResource)){
+        if (!count($accountResource)) {
             return array(
                 "status" => "ERROR",
                 "message" => "Currency not found"
             );
         }
 
-        $path = "accounts/". $accountResource[0]['value']."/addresses";
-        $result = $this->post($path,array(
+        $path = "accounts/" . $accountResource[0]['value'] . "/addresses";
+        $result = $this->post($path, array(
             "name" => $addressName
         ));
 
         $result = json_decode($result);
         $addresses = $result->data;
-        
+
 
         return array('status' => 'SUCCESS', 'data' => $addresses);
 
     }
 
-    function getAddressTransactions($account_id, $address_id){
-        $path = "accounts/".$account_id."/addresses/" . $address_id . "/transactions";
+    function getAddressTransactions($account_id, $address_id)
+    {
+        $path = "accounts/" . $account_id . "/addresses/" . $address_id . "/transactions";
+
         $result = $this->get($path);
 
-        return array('status'=>'SUCCESS','data'=> $result);
+        return array('status' => 'SUCCESS', 'data' => $result);
     }
 
 
     // ================ UTILITIES =======================
     //=================== DO NOT TOUCH UNLESS U KNOW WHAT YOU ARE DOING. =================
-    function post($path,$fields){
+    function post($path, $fields)
+    {
         $url = $this->endPoint . $path;
         $ch = curl_init($url);
         $fieldStr = "";
         $i = 0;
-        foreach($fields as $key => $value){
-            if($i > 0){
+        foreach ($fields as $key => $value) {
+            if ($i > 0) {
                 $fieldStr .= "&";
             }
-            $fieldStr .= $key."=".$value;   
+            $fieldStr .= $key . "=" . $value;
             $i++;
         }
 
-        $header = $this->getHeaders('POST',$path,$fieldStr);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $header = $this->getHeaders('POST', $path, $fieldStr);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch,CURLOPT_POST,1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$fieldStr);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldStr);
         //execute post
         $result = curl_exec($ch);
         //close connection
@@ -128,44 +137,82 @@ class GDAX extends CI_Model{
     }
 
 
-    function get($path){
+    function get($path)
+    {
         $url = $this->endPoint . $path;
         $ch = curl_init($url);
-        
-        $header = array();
-        $header = $this->getHeaders('GET',$path);
 
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        $header = array();
+        $header = $this->getHeaders('GET', $path);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         $res = curl_exec($ch);
         curl_close($ch);
 
         return $res;
-        
+
     }
 
 
 
 
- 
 
-    function sign($timestamp, $method, $path, $body){
-        $str = $timestamp . strtoupper($method) . $this->requestPath.$path . $body;
-        $hash = hash_hmac('sha256',$str,$this->apiSecret);
+
+    function sign($timestamp, $method, $path, $body)
+    {
+        $str = $timestamp . strtoupper($method) . $this->requestPath . $path . $body;
+        $hash = hash_hmac('sha256', $str, $this->apiSecret);
 
         return $hash;
     }
 
-    function getHeaders($method, $path, $body = ''){
+    function getHeaders($method, $path, $body = '')
+    {
         $timestamp = strtotime('now');
         $sign = $this->sign($timestamp, $method, $path, $body);
         $header = array(
-            "CB-ACCESS-KEY: ". $this->apiKey,
-            "CB-ACCESS-SIGN: ". $sign,
+            "CB-ACCESS-KEY: " . $this->apiKey,
+            "CB-ACCESS-SIGN: " . $sign,
             "CB-ACCESS-TIMESTAMP: " . $timestamp,
             "CB-VERSION: 2018-05-20",
             "ContentType: application/json"
         );
         return $header;
+    }
+
+    function external_transaction_process($transfer,$account_resource_id, $resource_id)
+    {
+        $this->db->select("*");
+        $this->db->from("deposit_address_transaction");
+        $this->db->where("transaction_id", $transfer["id"]);
+
+        $query = $this->db->get();
+
+        $result = $query->result_array();
+
+        if(empty($result)){
+            $data = array(
+                "transaction_id" => $transfer["id"],
+                "created_at" => $transfer["created_at"],
+                "updated_at" => $transfer["updated_at"],
+                "network" => $transfer["network"]["hash"],
+                "resource" => $account_resource_id,
+                "resource_path" => $transfer["resource_path"],
+                "currency" => $transfer["amount"]["currency"],
+                "amount" => $transfer["amount"]["amount"],
+                "type" => $transfer["type"],
+                "json" => json_encode($transfer)
+            );
+
+            $this->db->insert("deposit_address_transaction", $data);
+
+            $data = array(
+                "active" => 0
+            );
+
+            $this->db->where("resource_id", $resource_id);
+            $this->db->update("user_deposit_address", $data);
+        }
     }
 }
